6fb23de47807af0b2d0d8961b74ca316f20f7067
HansArne04 <hansarnemid@gmail.com>
2025-09-18 13:31:27 +0200

    test

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -1,17 +1,16 @@
 package no.hvl.dat100.tabeller;
 
 public class Tabeller {
-
-
-
+    public static void main(String[] args) {
+        int[] tab = {1,2,3,4,5};
+    }
 	// a)
-	public static void skrivUt(int[] tabell) {
+	public static int[] skrivUt(int[] tabell) {
 
-		
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+        
 
-	}
-
+    }
+/*
 	// b)
 	public static String tilStreng(int[] tabell) {
 
@@ -62,4 +61,5 @@ public class Tabeller {
 		throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
 
 	}
+	*/
 }
