9fa4db6a2bcf58e0d82d393f2591fe0b85236d83
190331-N <190331@hvl.no>
2025-09-18 13:01:04 +0200

    test 2

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -2,10 +2,12 @@ package no.hvl.dat100.tabeller;
 
 public class Tabeller {
 
+
+
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
+		
 		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
 
 	}
