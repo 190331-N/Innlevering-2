df2bc61b082470078870c61d562fc71dcf6435d3
190331-N <190331@hvl.no>
2025-09-23 13:16:46 +0200

    abc

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -1,15 +1,17 @@
-import java.util.Arrays;
 package no.hvl.dat100.tabeller;
-
+import java.util.Arrays;
 
 public class Tabeller {
     public static void main(String[] args) {
         int[] tab = {1, 2, 3, 4, 5};
         skrivUt(tab);
         System.out.println();
+        System.out.print(tilStreng(tab));
+        System.out.println();
         summer(tab);
         System.out.print(finnesTall(tab, 5));
         System.out.println();
+
 
     }
 
@@ -17,44 +19,44 @@ public class Tabeller {
     // a)
     public static void skrivUt(int[] tabell) {
         for (int tab : tabell) {
-            System.out.print(tab+", ");
+            System.out.print(tab + ", ");
         }
     }
 
-	// b)
-	public static String tilStreng(int[] tabell) {
-            for (int t : tabell) {
-                System.out.print(t + " ");
-            }
-            System.out.println();
-            }
-        }
+    // b)
+    public static String tilStreng(int[] tab) {
+            return Arrays.toString(tab).replace(" ", "");
+
+    }
 
 
-	}
-
-	// c)
-	public static int summer(int[] tabell) {
-        int sum=0;
+    // c)
+    public static int summer(int[] tabell) {
+        int sum = 0;
         for (int i = 0; i < tabell.length; i++) {
             sum += tabell[i];
         }
         System.out.println(sum);
         return sum;
 
-	}
+    }
 
-	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
+    // d)
+    public static boolean finnesTall(int[] tabell, int tall) {
 
         boolean tf = false;
         for (int i = 0; i < tabell.length; i++) {
-            if (tall == tabell[i]){
+            if (tall == tabell[i]) {
                 tf = true;
             }
         }
         return tf;
-	}
+    }
+}
+
+
+
+
 
     /*
 
