621506596ec53b4f2e63598e593d1430f121f1ba
HansArne04 <hansarnemid@gmail.com>
2025-09-30 13:49:20 +0200

    gg

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -74,24 +74,6 @@ public class Matriser {
                 }
             }
         }
-            return x;
-        }
-
-
-	// e)
-	public static int[][] speile(int[][] matrise) {
-
-		// TODO
-
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-
-	}
-
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-
-	}
+        return x;
+    }
 }
