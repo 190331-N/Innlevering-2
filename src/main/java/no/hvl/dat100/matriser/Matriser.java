a82e7c0c915b454520fe18409daa1e380602abd2
HansArne04 <hansarnemid@gmail.com>
2025-09-30 13:52:27 +0200

    l

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -3,9 +3,9 @@ package no.hvl.dat100.matriser;
 public class Matriser {
     public static void main(String[] args) {
         int[][] mat = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}
+                {1, 2, 3},
+                {5, 6, 7},
+                {9, 10, 12}
         };
 
         skrivUt(mat);
